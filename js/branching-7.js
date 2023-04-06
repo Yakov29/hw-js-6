fa6e5f89f55d65fe6ee72e34040bd98ac1892531
JakeDer29 <108018683+Yakov29@users.noreply.github.com>
2023-04-06 16:10:40 +0300

    Update branching-7.js

--- js/branching-7.js
+++ js/branching-7.js
@@ -11,8 +11,8 @@ age = 55;
 
 if (age > 0 && age <= 16) {
     console.log('Ти ще дитина');
-} else if (age >= 17 && age <= 60) {
+} else if (age >= 18 && age <= 60) {
     console.log('Ти дорослий');
-} else if (age >= 61 && age <= 100) {
-    console.log('Ти вже пенсіонер');
+} else if (age >= 66 && age <= 100) {
+    console.log('Ти вже старый:(.');
 };
